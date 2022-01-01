515510061bde854c5f97cfd396a24433a4710972
KawaiiAmber <japaneselearning101@gmail.com>
2022-01-01 00:26:24 -0700

    update

--- main.sh
+++ main.sh
@@ -1,41 +1,5 @@
 #!/bin/sh
 
-# Initialize empty vars.
-for var in PKGSDIR REPO REPO_BRANCH; do
-	eval $var=
-done
-
-# Initialize flags.
-for flag in HELP SKIP WARNING; do
-	eval $flag=false
-done
-
-# Read config if it exists.
-# ORDER: global, local, current directory
-[ -f /etc/ppmpss/ppmpss.rc ] && . /etc/ppmpss/ppmpss.rc
-[ -f ${XDG_CONFIG_HOME:-~/.config}/ppmpss/ppmpss.rc ] &&
-	. ${XDG_CONFIG_HOME:-~/.config}
-[ -f ./ppmpss.rc ] && . ./ppmpss.rc
-
-# Store non-option arguments.
-args=
-while [ $# -gt 0 ]; do
-	case $1 in
-	--[Pp]kgs_[Dd]ir=* | --[Pp]kgs_[Dd]irectory=*) PKGSDIR=${1#*=};;
-	--[Pp]kgs_[Dd]ir | --[Pp]kgs_[Dd]irectory) PKGSDIR=$2; shift;;
-	--[Rr]epo=* | --[Rr]epository=*) REPO=${1#*=};;
-	--[Rr]epo | --[Rr]epository) REPO=$2; shift;;
-	-[Hh] | --[Hh]elp) HELP=true;;
-	-[Yy] | --[Yy]es) SKIP=true;;
-	--) break;;
-	-*) printf "Unknown option: %s\n" "$1"; exit 1;;
-	*) args="$args $1";;
-	esac
-	shift
-done
-# Splitting is desired here.
-# shellcheck disable=SC2086
-set -- $args
 
 # Print a message:
 # msg normal "This is a normal message"
@@ -115,75 +79,134 @@ _EOF
 }
 
 # Prepare the package template and set warnings if needed.
-do_prepare() {
+# package_prepare sbase ->
+# pkg_src = $PKGSDIR/sbase
+package_prepare() {
+	msg normal "Preparing $1..."
 	pkg_src=$PKGSDIR/$1
 	. $pkg_src/template ||
 		msg error "Failed to parse template for $1" 3
 	[ $build_style = meta ] && short_desc="$pkg_src - meta package"
-	[ -z "$license" ] && LICENSE_WARN=true
-	[ -z "$revision" ] && [ $build_style != meta ] && REVISION_WARN=true
+	[ -z "$license" ] && [ $build_style != meta ] && LICENSE_WARN=true
+	[ -z "$revision" ] && REVISION_WARN=true
 	[ -z "$build_style" ] && [ -z "$(command -v do_install)" ] &&
+		[ $build_style != meta ] &&
 		msg error "No build_style or do_install() specified" 4
 }
 
 # Get the source, can be overriden.
-[ -z "$(command -v do_fetch)" ] && {
-	do_fetch() {
-		[ -z "$distfiles" ] && [ -z "$giturl" ] &&
-			msg error "No distfiles or giturl specified" 3
+package_fetch() {
+	msg normal "Fetching $1..."
+	[ -n "$(command -v do_fetch)" ] && { do_fetch; return; }
+	[ -z "$distfiles" ] && [ -z "$giturl" ] &&
+		msg error "No distfiles or giturl specified" 3
+	[ ${1#*-} = git ] && isGit=true || isGit=false
+	[ isGit = true ] && [ -z "$giturl" ] && {
+		msg warning "There is no git version available for ${1#*-}."
+		prompt "Use release $version"
+		isGit=false
 	}
+	[ isGit = true ] || [ -z "$distfiles" ] && {
+		git clone "$giturl" && [ -n "$commit" ] &&
+			git checkout "$commit"
+		return
+	}
+	curl -LO "$distfiles"
 }
 
-[ -z "$(command -v do_extract)" ] && {
-	do_extract() {
-		case ${1##*.} in
-			gzip | gz | tgz) gzip -d $1;;
-			xz | lzma) lzma -d $1;;
-		esac
-	}
+package_extract() {
+	msg normal "Extracting source for $1..."
+	[ -n "$(command -v do_extract)" ] && { do_extract; return; }
+	case ${1##*.} in
+		gzip | gz | tgz) gzip -d $1;;
+		xz | lzma) lzma -d $1;;
+	esac
 }
 
-# Configure the source before being built, can be overriden.
-# Initialize variables.
-: "${configure_script:=configure}"
-: "${configure_args:=--prefix=/usr}"
-[ -z "$(command -v do_configure)" ] && {
-	do_configure() {
-		case $build_style in
-		makefile) :;;
-		configure) sh $configure_script $configure_args;;
-		meta) :;;
-		*) msg error "Unknown build_style: $build_style" 2;;
-		esac
-	}
+# Configure the source before being built, can be replaced with do_configure
+# in templates.
+package_configure() {
+	msg normal "Configuring $1..."
+	# Initialize variables, can be used in do_configure functions.
+	: "${configure_script:=configure}"
+	: "${configure_args:=--prefix=/usr}"
+	[ -n "$(command -v do_configure)" ] && { do_configure; return; }
+	case $build_style in
+	makefile) :;;
+	configure) sh $configure_script $configure_args;;
+	meta) :;;
+	*) msg error "Unknown build_style: $build_style" 2;;
+	esac
 }
 
 # Build the package, can be overriden.
-# Initialize variables.
-: "${make_build_args:=CC=$CC CXX=$CXX}"
 # Splitting is desired here.
 # shellcheck disable=SC2086
-[ -z "$(command -v do_build)" ] && {
-	do_build() {
-		case $build_style in
-		makefile | configure) make $make_build_args;;
-		meta) :;;
-		*) msg error "Unknown build_style: $build_style" 2;;
-		esac
-	}
+package_build() {
+	msg normal "Building $1..."
+	# Initialize variables, can be used in do_build functions.
+	: "${make_build_args:=CC=$CC CXX=$CXX}"
+	[ -n "$(command -v do_build)" ] && { do_build; return; }
+	case $build_style in
+	makefile | configure) make $make_build_args;;
+	meta) :;;
+	*) msg error "Unknown build_style: $build_style" 2;;
+	esac
 }
 
 # Install package to empty DESTDIR, can be overriden.
 # Splitting is desired here.
 # shellcheck disable=SC2086
-: "${make_install_args:=PREFIX=$PREFIX DESTDIR=$DESTDIR}"
-[ -z "$(command -v do_install)" ] && {
-	do_install() {
-		case $build_style in
-		makefile | configure) make $make_install_args install;;
-		esac
-	}
+package_install() {
+	msg normal "Packaging $1..."
+	: "${make_install_args:=PREFIX=$PREFIX DESTDIR=$DESTDIR}"
+	[ -n "$(command -v do_install)" ] && { do_install; return; }
+	case $build_style in
+	makefile | configure) make $make_install_args install;;
+	esac
 }
+
+# Initialize empty vars.
+for var in PKGSDIR\
+	REPO\
+	REPO_BRANCH
+do
+	eval $var=
+done
+
+# Initialize flags.
+for flag in HELP SKIP WARNING; do
+	eval $flag=false
+done
+
+# Read config if it exists.
+# ORDER: global, local, current directory
+for conf in /etc/ppmpss/ppmpss.rc\
+	${XDG_CONFIG_HOME:-~/.config}/ppmpss/ppmpss.tc\
+	./ppmpss.rc
+do
+	[ -f $conf ] && . $conf
+done
+
+# Store non-option arguments.
+args=
+while [ $# -gt 0 ]; do
+	case $1 in
+	--[Pp]kgs_[Dd]ir=* | --[Pp]kgs_[Dd]irectory=*) PKGSDIR=${1#*=};;
+	--[Pp]kgs_[Dd]ir | --[Pp]kgs_[Dd]irectory) PKGSDIR=$2; shift;;
+	--[Rr]epo=* | --[Rr]epository=*) REPO=${1#*=};;
+	--[Rr]epo | --[Rr]epository) REPO=$2; shift;;
+	-[Hh] | --[Hh]elp) HELP=true;;
+	-[Yy] | --[Yy]es) SKIP=true;;
+	--) break;;
+	-*) msg error "Unknown option: $1" 1;;
+	*) args="$args $1";;
+	esac
+	shift
+done
+# Splitting is desired here.
+# shellcheck disable=SC2086
+set -- $args
 
 # Handle --help flag / empty arguments.
 [ $HELP = true ] && {
@@ -200,6 +223,7 @@ case $1 in
 	# TODO: Code it
 	[ $# -eq 0 ] && usage pkg
 	for arg; do (
+		package_prepare $arg
 		[ -n "$deps" ] && $0 pkg $deps
 		# Start packaging code here
 		)
